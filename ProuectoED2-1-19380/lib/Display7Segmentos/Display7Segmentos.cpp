14baf47ed70421db683c14d670841a92f320f933
Jose031 <90293076+Jose031@users.noreply.github.com>
2021-09-08 12:06:39 -0600

    Configuración de los segmentos de los displays
    Se llama a la función definida en el header y se establece que los pines de los displays son salidas, al igual se definen los valores iniciales de los segmentos de los displays.

--- ProuectoED2-1-19380/lib/Display7Segmentos/Display7Segmentos.cpp
+++ ProuectoED2-1-19380/lib/Display7Segmentos/Display7Segmentos.cpp
@@ -10,3 +10,39 @@ uint8_t pindisplayE;
 uint8_t pindisplayF;
 uint8_t pindisplayG;
 uint8_t pindisplaydP;
+
+//**********************************************************************************************************************
+// Prototipo de Funciones
+//**********************************************************************************************************************
+// Funcion de configuracion de display de 7 segmentos
+void configurarDisplay(uint8_t pinA, uint8_t pinB, uint8_t pinC, uint8_t pinD, uint8_t pinE, uint8_t pinF, uint8_t pinG, uint8_t pindP)
+{
+    //Actualizacion de variables
+    pindisplayA = pinA;
+    pindisplayB = pinB;
+    pindisplayC = pinC;
+    pindisplayD = pinD;
+    pindisplayE = pinE;
+    pindisplayF = pinF;
+    pindisplayG = pinG;
+    pindisplaydP = pindP;
+
+    //Definicion de salidas de los pines del display
+    pinMode(pindisplayA, OUTPUT);
+    pinMode(pindisplayB, OUTPUT);
+    pinMode(pindisplayC, OUTPUT);
+    pinMode(pindisplayD, OUTPUT);
+    pinMode(pindisplayE, OUTPUT);
+    pinMode(pindisplayF, OUTPUT);
+    pinMode(pindisplayG, OUTPUT);
+    pinMode(pindisplaydP, OUTPUT);
+    // Estado inicial de los leds del display
+    digitalWrite(pindisplayA, LOW);
+    digitalWrite(pindisplayB, LOW);
+    digitalWrite(pindisplayC, LOW);
+    digitalWrite(pindisplayD, LOW);
+    digitalWrite(pindisplayE, LOW);
+    digitalWrite(pindisplayF, LOW);
+    digitalWrite(pindisplayG, LOW);
+    digitalWrite(pindisplaydP, LOW);
+}
