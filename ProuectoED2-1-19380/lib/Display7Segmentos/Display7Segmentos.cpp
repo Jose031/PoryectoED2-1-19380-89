14a62c100617e5c8a48d5512ee1ded07e6b65d94
Jose031 <90293076+Jose031@users.noreply.github.com>
2021-09-08 12:04:03 -0600

    Creación de librería source
    Como primer paso se llama a la librería header , luego se llaman a las variables globales. Está librería igualmente es para el funcionamiento de los displays.

---
+++ ProuectoED2-1-19380/lib/Display7Segmentos/Display7Segmentos.cpp
@@ -0,0 +1,12 @@
+#include "Display7Segmentos.h"
+//**********************************************************************************************************************
+// Variables Globales
+//**********************************************************************************************************************
+uint8_t pindisplayA;
+uint8_t pindisplayB;
+uint8_t pindisplayC;
+uint8_t pindisplayD;
+uint8_t pindisplayE;
+uint8_t pindisplayF;
+uint8_t pindisplayG;
+uint8_t pindisplaydP;
